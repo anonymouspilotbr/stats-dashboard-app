a88a71f354910a428a1f9ba6d7ba4a0286f0bd4c
anonymouspilotbr <ybrasileiro6@gmail.com>
2026-06-08 11:40:16 -0300

    [v007] Updated styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { DashServerStatusComponent } from './dashboard/dashboard-server-status/dashboard-server-status.component';
 import { ServerTrafficComponent } from "./dashboard/server-traffic/server-traffic.component";
 import { SupportTicketsComponent } from "./dashboard/support-tickets/support-tickets.component";
 import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
+import { DashServerStatusComponent } from './dashboard/dashboard-server-status/dashboard-server-status.component';
 
 @Component({
   selector: 'app-root',
